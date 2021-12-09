62405704998a5eb891142b105f1369fb5408031d
vinaymaruri <vmaruri1@berkeley.edu>
2021-12-08 18:31:30 -0600

    modular solution

--- day-6/day-6.py
+++ day-6/day-6.py
@@ -2,48 +2,38 @@ with open("input.txt", 'r') as file:
     f = file.readlines()
 file.close()
 
+#create the initial list of laternfish.
 lfish = [int(c) for c in f[0].split(",")]
 
-print(len(lfish))
-#80 days in total.
-#part 1 solution: brute force [this is a terribad algorithm..n! runtime]
-days_elapsed = 0
-new_fish_cnt = 0
-while days_elapsed < 80:
-    for elem in range(len(lfish)):
-        if lfish[elem] == 0:
-            lfish[elem] = 6
-            new_fish_cnt += 1
-        else:
-            lfish[elem] = lfish[elem] - 1
-    if new_fish_cnt > 0:
-        for i in range(new_fish_cnt):
-            lfish.append(8)
-        new_fish_cnt = 0
-    days_elapsed += 1
 
-#print(len(lfish))
-
-#part 2 solution: using dictionaries which is much faster.
-
-start = lfish                                  
-new = {}                                                              
-for i in start:
-    new[i] = new[i]+1 if i in new else 1
-                                    
-start = new
-for day in range(256):
-    new = {}
-    for fish in start:
-        timer = fish-1
-        if timer == -1:
-            new[8] = start[fish]
-            new[6] = start[fish]+new[6] if 6 in new else start[fish]
-        else:
-            new[timer] = start[fish]+new[timer] if timer in new else start[fish]
-
-    start = new
-
-print(sum(start.values()))
+#modular solution now that I got the split solution.
+#idea: this solution merely tracks fish as they shift between timer bins using a dictionary
+#that way the data structure doesn't expand exponentially to an unreasonable size as it would
+#with a list.
+#inputs: fish_lst is the initial list of lanternfish, days is the number of days to simulate
+#output: returns a dictionary representing the amount of fish in each timer bin.
+def modular_solution(fish_lst, days):
+    init = fish_lst                                 
+    new = {}                                                              
+    for i in init:
+        new[i] = new[i]+1 if i in new else 1                                   
+    counts = new
+    for _ in range(days):
+        new = {}
+        for fish in counts:
+            timer = fish-1
+            if timer == -1:
+                new[8] = counts[fish]
+                new[6] = counts[fish]+new[6] if 6 in new else counts[fish]
+            else:
+                new[timer] = counts[fish]+new[timer] if timer in new else counts[fish]
+        counts = new
+    return counts
+#part 1 solution
+part_1 = modular_solution(lfish, 80)
+#part 2 solution
+part_2 = modular_solution(lfish, 256) 
+print(sum(part_1.values()))
+print(sum(part_2.values()))
 
 
